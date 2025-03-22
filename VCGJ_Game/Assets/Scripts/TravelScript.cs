926b1818d3c04b16f3dd24ac49fee271bb6f1b8f
Elijah G <kmgilliam@wm.edu>
2025-03-22 17:34:40 -0400

    Add ability to move between scenes
    Add "goal" tag and player interaction with "goal" objects upon collision (needs to be generalized)

--- VCGJ_Game/Assets/Scripts/TravelScript.cs
+++ VCGJ_Game/Assets/Scripts/TravelScript.cs
@@ -2,20 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.ProjectWindowCallback;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TravelScript : MonoBehaviour
 {
     private GameObject player;
     private Vector3 endPosition;
-    private GameObject gapExit;
+    private GameObject exit;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         player = GameObject.Find("Player");
         if (collision.collider.tag == "Gap")
         {
-            gapExit = GameObject.FindWithTag("GapExit"); // assuming that there will be at most one gap/exit pair per level
-            endPosition = gapExit.transform.position;
+            exit = GameObject.FindWithTag("GapExit"); // assuming that there will be at most one gap/exit pair per level
+            endPosition = exit.transform.position;
 
             if (player.GetComponent<PlayerMovement>().canSqueeze())
             {
@@ -23,6 +24,12 @@ public class TravelScript : MonoBehaviour
                 player.transform.rotation = player.transform.rotation;
             }
 
+        }
+        else if (collision.collider.tag == "Goal")
+        {
+            exit = GameObject.FindWithTag("Goal");
+            Debug.Log("Goal!");
+            SceneManager.LoadScene("PlayerTest");
         }
     }
 }
